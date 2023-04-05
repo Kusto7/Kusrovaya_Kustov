fc2cbb1d6629ee00f7db005c859148e9653dbc09
Kusto7 <kirill.kustov21@gmail.com>
2023-04-05 20:28:05 +0300

    Открыт файл, добавлен

--- main/main.py
+++ main/main.py
@@ -1,2 +1,10 @@
+import json
+
+
+with open("operations.json", 'r', encoding="utf-8") as file:
+    raw_json = file.read()
+    all_operations = json.loads(raw_json)
 def operations_data():
     pass
+
+print(all_operations)
