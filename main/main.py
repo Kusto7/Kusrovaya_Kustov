66bf2ca89c53eefed9f0d69bf59da26d1d990d01
Kusto7 <kirill.kustov21@gmail.com>
2023-04-07 20:59:38 +0300

    Занёс под функцию, добавил функционал вывода кол-во операций. Начинаю делать тесты.

--- main/main.py
+++ main/main.py
@@ -1,55 +1,62 @@
 import json
 
-with open("operations.json", 'r', encoding="utf-8") as file:
-    raw_json = file.read()
-    all_operations = json.loads(raw_json)
-
 
 def operations_data():
-    pass
+    with open("operations.json", 'r', encoding="utf-8") as file:
+        raw_json = file.read()
+        all_operations = json.loads(raw_json)
 
-
-last_operations = 5
-
-max_data = []
-for operation in all_operations:
-    if "date" in operation:
-        if operation["state"] == "EXECUTED":
-            data_operations = operation['date']
-            max_data.append(data_operations)
+    max_data = []
+    for operation in all_operations:
+        if "date" in operation:
+            if operation["state"] == "EXECUTED":
+                data_operations = operation['date']
+                max_data.append(data_operations)
+            else:
+                continue
         else:
             continue
-    else:
-        continue
 
-sorted_data = sorted(max_data)
-for operation in all_operations:
-    if "date" in operation:
-        if operation['date'] in sorted_data[:-5-1:-1]:
-            if last_operations == 0:
-                break
-            else:
-                print('')
-                name_to = f"{''.join(i for i in operation['to'].split(' ')[:-1])} " \
-                          f"**{''.join(i for i in operation['to'].split(' ')[-1][-4:])}"
-                summ_amount = operation['operationAmount']['amount']
-                currency_name = operation['operationAmount']['currency']['name']
-                date_ = f"{operation['date'][0:4]}.{operation['date'][5:7]}.{operation['date'][8:10]}"
-                description = operation['description']
-                if "from" in operation:
-                    data_from = operation["from"].split(' ')
-                    if len(data_from[-1]) == 16:
-                        name_from = " ".join(i for i in data_from[:-1])
-                        number_from = f"{''.join(i for i in data_from[-1][0:4])} " \
-                                      f"{''.join(i for i in data_from[-1][4:6])}" \
-                                      f"** **** {''.join(i for i in data_from[-1][-4:])}"
-                    else:
-                        name_from = " ".join(i for i in data_from[:-1])
-                        number_from = f"**{''.join(i for i in data_from[-1][-4:])}"
-                else:
-                    name_from = None
-                    number_from = ''
+    sorted_data = sorted(max_data)
+    last_operations = 1
+    user_need_operation = int(input("Сколько последних выполненных операций показать? "))
+    while True:
+        if user_need_operation == 0:
+            break
+        else:
+            for operation in all_operations:
+                if "date" in operation:
+                    if operation['date'] in sorted_data[-last_operations]:
+                        print('')
+                        name_to = f"{''.join(i for i in operation['to'].split(' ')[:-1])} " \
+                                  f"**{''.join(i for i in operation['to'].split(' ')[-1][-4:])}"
+                        summ_amount = operation['operationAmount']['amount']
+                        currency_name = operation['operationAmount']['currency']['name']
+                        date = f"{operation['date'][8:10]}.{operation['date'][5:7]}.{operation['date'][0:4]}"
+                        description = operation['description']
+                        if "from" in operation:
+                            data_from = operation["from"].split(' ')
+                            if len(data_from[-1]) == 16:
+                                name_from = " ".join(i for i in data_from[:-1])
+                                number_from = f"{''.join(i for i in data_from[-1][0:4])} " \
+                                              f"{''.join(i for i in data_from[-1][4:6])}" \
+                                              f"** **** {''.join(i for i in data_from[-1][-4:])}"
+                            else:
+                                name_from = " ".join(i for i in data_from[:-1])
+                                number_from = f"**{''.join(i for i in data_from[-1][-4:])}"
+                        else:
+                            name_from = None
+                            number_from = None
+                        if name_from:
+                            print(f"{date} {description} \n{name_from} {number_from}"
+                                  f" -> {name_to}\n{summ_amount} {currency_name}")
+                            user_need_operation -= 1
+                            last_operations += 1
+                        else:
+                            print(f"{date} {description}\n"
+                                  f"{name_to}\n{summ_amount} {currency_name}")
+                            user_need_operation -= 1
+                            last_operations += 1
 
-            print(f'''{date_} {description}
-{name_from} {number_from} -> {name_to}
-{summ_amount} {currency_name}''')
+
+operations_data()
